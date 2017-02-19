e91c48f969b35d8ba4e496f0901c18c4ebdd9349
brettsnaidero <brettsnaidero@hotmail.com>
2017-02-19 22:36:09 +1100

    Showcase page

--- src/shared/components/App/Showcase/showcase.js
+++ src/shared/components/App/Showcase/showcase.js
@@ -4,14 +4,22 @@ import React, { Component } from 'react';
 import * as _ from "lodash";
 
 import showcaseItems from '../data/showcase-items.js';
+import Shapes from '../bits/shapes';
 
 export default class WorkPage extends Component {
 	constructor(props) {
         super(props);
+
+		this.state = {
+			page: {}
+		}
     }
 
 	componentDidMount() {
-
+		let page = _.find(showcaseItems, {'id': this.props.params.id});
+		this.setState({
+			page: page
+		})
 	}
 
 	render() {
@@ -21,34 +29,44 @@ export default class WorkPage extends Component {
 				<section
 				className="work-banner"
 				style={{
-					backgroundImage: 'url(../img/example.jpg)'
+					backgroundImage: 'url(../' + this.state.page.image + ')'
 				}}>
 					<div className="row">
-						<div className="text">
-							<div className="client">Client: Myself!</div>
-							<h1>For the common good</h1>
-							<p>Project: Visual Identity, Website, Interior<br />Unit: e-Types Daily, e-Types Digital, e-Types Studio, Playtype</p>
+						<div className="text iseeyou">
+							<div className="client">Client: {this.state.page.title}</div>
+							<h1>{this.state.page.title}</h1>
 						</div>
 					</div>
 				</section>
 				{/* Content */}
 				<section className="work-content">
 					<div className="row">
-						<div className="text">
-							<div className="client">Client: Myself!</div>
-							<h1>For the common good</h1>
-							<p>Project: Visual Identity, Website, Interior<br />Unit: e-Types Daily, e-Types Digital, e-Types Studio, Playtype</p>
+						<div className="text iseeyou">
+							<p>The <strong>British Airways</strong> lounges at Heathrow Airport have a combined footfall of over 155,000 every month. That’s almost two million people that pass through these areas every year. BA approached us requiring a way of advertising to their passengers using digital signage.</p>
+						</div>
+						<div className="side">
+							<div className="bit iseeyou">
+								<div className="title">Client</div>
+								<div>British Airways</div>
+							</div>
+							<div className="bit iseeyou">
+								<div className="title">Expertise</div>
+								<div>Apps, Strategy, Security</div>
+							</div>
 						</div>
 					</div>
 				</section>
 				{/* Images */}
 				<section className="work-images">
 					<div className="row">
-						<div className="text">
-							<div className="client">Client: Myself!</div>
-							<h1>For the common good</h1>
-							<p>Project: Visual Identity, Website, Interior<br />Unit: e-Types Daily, e-Types Digital, e-Types Studio, Playtype</p>
-						</div>
+						<img className="iseeyou" src={"../" + this.state.page.image} />
+					</div>
+				</section>
+				<section className="work-contact">
+					<Shapes />
+					<div className="row">
+						<h2>That was a fun project :)</h2>
+						<p>If you want to work with me on other fun projects, please get in touch.</p>
 					</div>
 				</section>
             </div>
