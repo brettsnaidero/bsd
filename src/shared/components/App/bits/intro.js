b090ff0076f579918fd6c515f6bda19baeb42316
brettsnaidero <brettsnaidero@hotmail.com>
2017-03-12 17:24:26 +1100

    Colours

--- src/shared/components/App/bits/intro.js
+++ src/shared/components/App/bits/intro.js
@@ -30,6 +30,17 @@ export default class Intro extends Component {
 
             {/* Designer */}
 			<ISeeYou classesForChild="banner-text">
+				<div className="strokereveal">
+					<svg x="0px" y="0px" viewBox="0 0 692.5 472.7">
+						<polyline className="st0" points="50.6,236.4 247.3,50 50,422.7 444.6,50 247.3,422.7 641.9,50 444.6,422.7 642.5,236.4 "/>
+						<defs>
+							<linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%" gradientTransform="rotate(0)">
+								<stop offset="0%" stopColor="#3a1fc3"/>
+								<stop offset="100%" stopColor="#0c1b78"/>
+							</linearGradient>
+						</defs>
+					</svg>
+				</div>
 				<h1>
 					<span className="line"><span className="top">Hello, I'm <span className="name">Brett Snaidero</span> —</span></span>
 					<span className="line"><span className="bottom">a <span className="job">Digital Designer</span> from <span className="city">Sydney</span> <span className="hash">*</span></span></span>
