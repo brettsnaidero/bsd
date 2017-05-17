0cc44ff73ea9346a5d81ad8bbc9e4bf0998d6c98
Brett Snaidero <brettsnaidero@hotmail.com>
2017-05-17 21:44:12 +1000

    Fading between routes

--- src/shared/components/App/App.js
+++ src/shared/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import Helmet from 'react-helmet';
 import { CodeSplit } from 'code-split-component';
@@ -34,25 +35,33 @@ function App() {
           <meta http-equiv="Content-Security-Policy" content="default-src 'self' https://www.youtube.com; options inline-script" />
         </Helmet>
 
-        <Switch>
-  	    	<Route
-  	    		exact
-  	    		path="/"
-  	    		component={Home}
-  	    	/>
+        <Route render={({ location }) => (
+          <ReactCSSTransitionGroup
+            transitionName="fade"
+            transitionEnterTimeout={300}
+            transitionLeaveTimeout={300}
+          >
+          <Switch key={location.key}>
+    	    	<Route
+    	    		exact
+    	    		path="/"
+    	    		component={Home}
+    	    	/>
 
-          <Route
-  	    		path="/writings/:id"
-  	    		component={Blog}
-  	    	/>
+            <Route
+    	    		path="/writings/:id"
+    	    		component={Blog}
+    	    	/>
 
-          <Route
-  	    		path="/work/:id"
-  	    		component={Work}
-  	    	/>
+            <Route
+    	    		path="/work/:id"
+    	    		component={Work}
+    	    	/>
 
-  	    	<Route component={NotFound} />
-        </Switch>
+    	    	<Route component={NotFound} />
+          </Switch>
+          </ReactCSSTransitionGroup>
+        )}/>
     </div>
   );
 }
